1432eeb823ed5b05ff95c0eb40e4a773faacb746
Guilherme Lenny <guilherme_kenay@outlook.com>
2024-05-24 21:48:54 -0300

    update

---
+++ poo_typescript/app.ts
@@ -0,0 +1,142 @@
+// Programação Orientada a Objetos com TypeScript
+// 1- O que é POO
+/*
+const user = {
+    name: 'Guilherme',
+    age: 27,
+    showName() {
+        console.log(this.name)
+    }
+}
+
+const otherUser = {
+    name: 'Geovana',
+    age: 24,
+    showName() {
+        console.log(this.name)
+    }
+}
+
+console.log(user.name) // Acessa o atributo do objeto
+user.showName() // Utilizando o objeto e método para mostrar o atributo
+otherUser.showName()
+*/
+
+class User {
+    name: string = 'Guilherme'
+    age: number = 27
+
+    constructor(name: string, age: number) {
+        this.name = name
+        this.age = age
+    }
+// O this quando passado dentro da classe ou método construtor, ele acessa a propriedade do objeto
+
+    showName = () => {
+        console.log(this.name)
+    }
+}
+// Em vez de criar objeto por objeto a classe é utilizada para instanciar a criação de varios objetos
+
+const user: User = new User('Guilherme', 27)
+user.showName()
+
+const otherUser = new User('Geovana', 24)
+otherUser.showName()
+
+// 2- Classes e tipos
+// Abstração : A abstração na POO é a capacidade de ocultar detalhes irrelevantes ou complexos de um problema e focar nos aspectos essenciais.
+
+//Dio Banking
+
+//name, accountNumber
+//depositar, sacar
+
+/*
+class Account {
+    name: string
+    accountNumber: number
+
+    constructor(name: string, accountNumber: number) {
+        this.name = name
+        this.accountNumber = accountNumber
+    }
+
+    deposit = () => {
+        console.log('Você depositou')
+    }
+
+    withdraw = () => {
+        console.log('Você sacou')
+    }
+}
+*/
+
+/*
+const newAccount: Account = new Account('Guilherme', 1)
+console.log(newAccount)
+
+const account: Account = new Account('Geovana', 2)
+account.deposit()
+*/
+
+// 3- Herança
+// extends faz que a classe Admin herde atributos e métodos da Account (dessa forma a Admin vira derivada da Account e necessita do super construtor)
+/*
+class Admin extends Account{
+    balance: number
+
+    constructor(name: string, accountNumber: number) {
+        super(name, accountNumber)
+        this.balance = 20
+    }
+
+    getBalance = () => {
+        console.log(this.balance)
+    }
+}
+
+const adminAccount: Admin = new Admin('Guilherme', 1)
+console.log(adminAccount)
+
+const account: Account = new Account('Geovana', 2)
+console.log(account)
+*/
+
+// Classe Abstrata: Uma classe abstrata é um tipo especial de classe em programação que não pode ser instanciada por si só. Em vez disso, ela serve como uma base para outras classes. Pense nela como um modelo ou uma blueprint que define métodos e propriedades comuns, mas deixa a implementação específica desses métodos para as subclasses que a herdam.
+abstract class Account {
+    name: string
+    accountNumber: number
+    balance: number = 0
+
+    constructor(name: string, accountNumber: number) {
+        this.name = name
+        this.accountNumber = accountNumber
+    }
+
+    deposit = () => {
+        console.log('Você depositou')
+    }
+
+    withdraw = () => {
+        console.log('Você sacou')
+    }    
+    
+    getBalance = () => {
+        console.log(this.balance)
+    }
+}
+
+class PeopleAccount extends Account {
+    doc_id:number
+
+    constructor(doc_id: number, name: string, accountNumber: number) {
+        super(name, accountNumber)
+        this.doc_id = doc_id
+    }
+}
+
+const peopleAccount: PeopleAccount = new PeopleAccount(10, 'Guilherme', 1)
+console.log(peopleAccount)
+
+// 4- Módulos
